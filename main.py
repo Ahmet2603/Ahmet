be0a6007fb98b66a7d8633c27e599a4c79906a30
Ahmet2603 <ahmet41kocaeli44@gmail.com>
2026-06-25 14:01:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,26 @@
 import streamlit as st
+import requests
 
-st.title("Ücretli Vergi Hesaplama (2026)")
-gelir=st.number_input("Gelir giriniz", min_value=0)
+url=f"https://api.coinlore.net/api/tickers/"
+data=requests.get(url).json()["data"]
 
-vergi=0
+fiyat={coin["symbol"]:float(coin["price_usd"]) for coin in data}
+coins=list(fiyat.keys())
 
-if gelir <=190_000:
-  vergi=gelir*0.15
+col1, col2, col3 = st.columns(3)
 
-elif gelir<=400_000:
-  vergi=((190_000*0.15)
-  +((gelir-190_000)*0.20))
+with col1:
+    gonderilen_coin=st.selectbox("Gönderilen coin",coins)
 
-elif gelir<=1_500_000:
-  vergi=((190_000*0.15)
-  +(210_000*0.20)
-  +((gelir-400_000)*0.27))
+with col2:
+    alinan_coin=st.selectbox("Alınan coin",coins)
 
-elif gelir<=5_300_000:
-  vergi=((190_000*0.15)
-  +(210_000*0.20)
-  +(1_100_000*0.27)
-  +((gelir-1_500_000)*0.35))
+with col3:
+    miktar=st.number_input("Miktar", value=1.0)
 
-else:
-  vergi=((190_000*0.15)
-  +(210_000*0.20)
-  +(1_100_000*0.27)
-  +(3_800_000*0.35)
-  +((gelir-5_300_000)*0.40))
-
-
-st.write("Vergi.",vergi)
+if st.button("Dönüştür"):
+    if gonderilen_coin in fiyat and alinan_coin in fiyat:
+        results=(fiyat[gonderilen_coin]/fiyat[alinan_coin]*miktar)
+        st.success(f"{miktar} {gonderilen_coin} = {results:6f} {alinan_coin}")
+    else:
+        st.error("Coin Bulunamadı")
